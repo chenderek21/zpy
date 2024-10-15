78e4873228db88ce817c5b88edf62d96e874cb39
Derek Chen <chen.derek21@gmail.com>
2024-10-15 16:24:49 -0700

    moved lobby to server

--- src/frontend/scripts/joinRoom.ts
+++ src/frontend/scripts/joinRoom.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-import { LobbyPlayer } from '../../shared/Lobby';
-import { Lobby } from '../../shared/Lobby';
+import { LobbyPlayer } from '../../backend/Lobby';
+import { Lobby } from '../../backend/Lobby';
 
 
 function getRoomCodeFromURL(): string {
